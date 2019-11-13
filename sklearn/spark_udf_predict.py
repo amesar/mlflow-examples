c32e718282d8e088023b85c92e2eac911477acca
amesar <amesar@users.noreply.github.co>
2019-11-13 01:41:33 +0000

    sklearn/spark_udf_predict.py tweaks

--- sklearn/spark_udf_predict.py
+++ sklearn/spark_udf_predict.py
@@ -5,7 +5,7 @@ Serve predictions with Spark UDF.
 import sys
 from pyspark.sql import SparkSession
 import mlflow
-import mlflow.sklearn
+import mlflow.pyfunc
 
 if __name__ == "__main__":
     path = sys.argv[2] if len(sys.argv) > 2 else "../data/wine-quality-white.csv"
@@ -16,18 +16,17 @@ if __name__ == "__main__":
 
     spark = SparkSession.builder.appName("ServePredictions").getOrCreate()
 
-    df = spark.read.option("inferSchema",True).option("header", True).csv(path) if path.endswith(".csv") \
+    data = spark.read.option("inferSchema",True).option("header", True).csv(path) if path.endswith(".csv") \
     else spark.read.option("multiLine",True).json(path)
 
-    if "quality" in df.columns:
-        df = df.drop("quality")
-    df.show(10)
+    if "quality" in data.columns:
+        data = data.drop("quality")
+    data.show(10)
 
     model_uri = f"runs:/{run_id}/sklearn-model"
     udf = mlflow.pyfunc.spark_udf(spark, model_uri)
-    df2 = df.withColumn("prediction", udf(*df.columns))
-    df2.show(10)
-    df2.select("prediction").show(10)
-    pred = df2.select("prediction").first()[0]
+    predictions = data.withColumn("prediction", udf(*data.columns))
+    predictions.show(10)
+    predictions.select("prediction").show(10)
+    pred = predictions.select("prediction").first()[0]
     print("predictions: {:,.7f}".format(pred))
-
