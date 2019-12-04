887cedd976fe0b84dc18dda84fbde13947b0239b
amesar <amesar@users.noreply.github.co>
2019-12-04 00:18:43 +0000

    pyspark: fixed mleap read bug

---
+++ pyspark/mleap_utils.py
@@ -0,0 +1,11 @@
+from pyspark.ml import PipelineModel
+from mleap.pyspark.spark_support import SimpleSparkSerializer
+
+"""
+- Unfortunately MLflow does not have an API method to read in a model as a Spark Bundle.
+- So we have to manually construct the bundle URI and directly deserialize it with MLeap methods.
+"""
+def read_model(run, artifact_path):
+    bundle_uri = f"file:{run.info.artifact_uri}/" + artifact_path
+    print("bundle_uri:",bundle_uri)
+    return PipelineModel.deserializeFromBundle(bundle_uri)
--- pyspark/predict.py
+++ pyspark/predict.py
@@ -2,6 +2,9 @@ from argparse import ArgumentParser
 import mlflow
 import mlflow.spark
 from pyspark.sql import SparkSession
+#from pyspark.ml import PipelineModel
+#from mleap.pyspark.spark_support import SimpleSparkSerializer
+import mleap_utils
 from common import *
 
 print("MLflow Version:", mlflow.version.VERSION)
@@ -23,18 +26,19 @@ if __name__ == "__main__":
     data.printSchema()
 
     # Predict with Spark ML
+    print("Spark ML predictions")
     model_uri = f"runs:/{args.run_id}/spark-model"
     print("model_uri:",model_uri)
     model = mlflow.spark.load_model(model_uri)
     predictions = model.transform(data)
     df = predictions.select(colPrediction, colLabel, colFeatures)
-    print("Spark ML predictions")
     df.show(5,False)
 
-    # Predict with MLeap 
-    model_uri = f"runs:/{args.run_id}/mleap-model"
-    print("model_uri:",model_uri)
+    # Predict with MLeap as SparkBundle
+    print("MLeap ML predictions")
+    client = mlflow.tracking.MlflowClient()
+    run = client.get_run(args.run_id)
+    model = mleap_utils.read_model(run, "mleap-model/mleap/model")
     predictions = model.transform(data)
     df = predictions.select(colPrediction, colLabel, colFeatures)
-    print("MLeap ML predictions")
     df.show(5,False)
