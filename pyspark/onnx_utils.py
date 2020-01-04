f1d7ce674473e85f5a978a128d5601ce330bdbad
amesar <amesar@users.noreply.github.co>
2020-01-04 00:49:46 +0000

    pyspark: change to use onnxmltools.convert.sparkml.utils. buildInitialTypesSimple for building input_types

--- pyspark/onnx_utils.py
+++ pyspark/onnx_utils.py
@@ -4,7 +4,8 @@ def log_model(spark, model, name, data_df):
     import mlflow.onnx
     import onnxmltools
     from onnxmltools.convert.common.data_types import FloatTensorType
-    initial_types = [ (col, FloatTensorType([1, 1])) for col in data_df.columns ]
+    from onnxmltools.convert.sparkml.utils import buildInitialTypesSimple
+    initial_types = buildInitialTypesSimple(data_df)
     onnx_model = onnxmltools.convert_sparkml(model, name, initial_types, spark_session=spark)
     mlflow.onnx.log_model(onnx_model, "onnx-model")
     mlflow.set_tag("onnx_version", onnx.__version__)
