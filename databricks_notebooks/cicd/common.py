a58032ee384e97d75670ec14d278ad693f65d2ef
Andre Mesarovic <amesar@users.noreply.github.com>
2020-03-31 21:37:25 -0400

    Databricks CICD: latest common.py cleanup

--- databricks_notebooks/cicd/common.py
+++ databricks_notebooks/cicd/common.py
@@ -18,3 +18,12 @@ def download_wine_file():
     data_uri = "https://raw.githubusercontent.com/mlflow/mlflow/master/examples/sklearn_elasticnet_wine/wine-quality.csv"
     download_file(data_uri, data_path)
     return data_path
+
+# COMMAND ----------
+
+#def get_user():
+  #user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().get("user").get()
+  #return user
+  
+def get_experiment_name():
+    return dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
