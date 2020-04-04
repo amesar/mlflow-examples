c413a010a1c2cfd088d22d452b0a670d78bd6a6b
amesar <amesar@users.noreply.github.co>
2020-04-04 04:20:00 +0000

    sklearn: add setup.py

---
+++ python/sklearn/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+  
+setup(name='mlflow-sklearn-wine',
+      version='0.0.1',
+      description='MLFlow sklearn wine quality example',
+      author='Andre',
+      packages=['wine_quality'],
+      zip_safe=False)
