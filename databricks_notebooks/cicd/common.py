f98ec7edfe29eaed52a527c53a258ac894f17edc
Andre Mesarovic <amesar@users.noreply.github.com>
2020-04-03 23:15:14 -0400

    Latest common notebook changes

--- databricks_notebooks/cicd/common.py
+++ databricks_notebooks/cicd/common.py
@@ -2,28 +2,19 @@
 import os
 import requests
 
-def download_file(data_uri, data_path):
-    if os.path.exists(data_path):
-        print("File {} already exists".format(data_path))
+def download_uri(uri, path):
+    if os.path.exists(path):
+        print(f"File {path} already exists")
     else:
-        print("Downloading {} to {}".format(data_uri,data_path))
-        rsp = requests.get(data_uri)
-        with open(data_path, 'w') as f:
-            f.write(requests.get(data_uri).text)
+        print(f"Downloading {uri} to {path}")
+        rsp = requests.get(uri)
+        with open(path, "w") as f:
+            f.write(rsp.text)
 
 # COMMAND ----------
 
 def download_wine_file():
     data_path = "/dbfs/tmp/mlflow_wine-quality.csv"
-    data_uri = "https://raw.githubusercontent.com/mlflow/mlflow/master/examples/sklearn_elasticnet_wine/wine-quality.csv"
-    download_file(data_uri, data_path)
+    uri = "https://raw.githubusercontent.com/mlflow/mlflow/master/examples/sklearn_elasticnet_wine/wine-quality.csv"
+    download_uri(uri, data_path)
     return data_path
-
-# COMMAND ----------
-
-#def get_user():
-  #user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().get("user").get()
-  #return user
-  
-def get_experiment_name():
-    return dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
