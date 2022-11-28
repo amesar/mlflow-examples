628262e984cb71e20ed5794f0d2f76a8b2498769
Andre Mesarovic <amesar@users.noreply.github.com>
2022-11-28 01:25:49 -0500

    Notebook 02a_Sklearn_Train_Predict: added --model-version-stage and --archive-existing-versions widget options

--- databricks/notebooks/models_basic/python/05a_Keras_Train_Predict.py
+++ databricks/notebooks/models_basic/python/05a_Keras_Train_Predict.py
@@ -14,7 +14,7 @@
 
 # COMMAND ----------
 
-dbutils.widgets.removeAll()
+# dbutils.widgets.removeAll()
 
 # COMMAND ----------
 
@@ -22,27 +22,34 @@ dbutils.widgets.removeAll()
 
 dbutils.widgets.text("1. Experiment Name","")
 dbutils.widgets.text("2. Registered Model","")
-dbutils.widgets.dropdown("3. Save Signature","no",["yes","no"])
-dbutils.widgets.dropdown("4. SHAP","no",["yes","no"])
-dbutils.widgets.text("5. Max Depth", "1") 
-dbutils.widgets.text("6. Max Leaf Nodes", "")
+dbutils.widgets.dropdown("3. Model version stage","None",["Production","Staging","Archived","None"])
+dbutils.widgets.dropdown("4. Achive existing versions","yes",["yes","no"])
+dbutils.widgets.dropdown("5. Save Signature","no",["yes","no"])
+dbutils.widgets.dropdown("6. SHAP","no",["yes","no"])
+dbutils.widgets.text("7. Max Depth", "1") 
+dbutils.widgets.text("8. Max Leaf Nodes", "")
 
 experiment_name = dbutils.widgets.get("1. Experiment Name")
-registered_model = dbutils.widgets.get("2. Registered Model")
-save_signature = dbutils.widgets.get("3. Save Signature") == "yes"
-shap = dbutils.widgets.get("4. SHAP") == "yes"
-max_depth = to_int(dbutils.widgets.get("5. Max Depth"))
-max_leaf_nodes = to_int(dbutils.widgets.get("6. Max Leaf Nodes"))
+model_name = dbutils.widgets.get("2. Registered Model")
+model_version_stage = dbutils.widgets.get("3. Model version stage")
+archive_existing_versions = dbutils.widgets.get("4. Achive existing versions") == "yes"
+save_signature = dbutils.widgets.get("5. Save Signature") == "yes"
+shap = dbutils.widgets.get("6. SHAP") == "yes"
+max_depth = to_int(dbutils.widgets.get("7. Max Depth"))
+max_leaf_nodes = to_int(dbutils.widgets.get("8. Max Leaf Nodes"))
 
-if registered_model=="": registered_model = None
+if model_name=="": model_name = None
+if model_version_stage=="None": model_version_stage = None
+if experiment_name=="None": experiment_name = None
 
 print("experiment_name:",experiment_name)
-print("registered_model:",registered_model)
+print("model_name:",model_name)
+print("model_version_stage:",model_version_stage)
+print("archive_existing_versions:",archive_existing_versions)
 print("save_signature:",save_signature)
 print("SHAP:",shap)
 print("max_depth:",max_depth)
 print("max_leaf_nodes:",max_leaf_nodes)
-print("save_signature:",save_signature)
 
 # COMMAND ----------
 
@@ -52,7 +59,7 @@ import mlflow.sklearn
 
 # COMMAND ----------
 
-if not experiment_name == "":
+if experiment_name:
     mlflow.set_experiment(experiment_name)
 
 # COMMAND ----------
@@ -100,9 +107,23 @@ from mlflow.models.signature import infer_signature
 
 # COMMAND ----------
 
-model_name = "sklearn-model"
+client = mlflow.tracking.MlflowClient()
 
-with mlflow.start_run(run_name="sklearn") as run:
+def _register_model(model_name, model_version_stage, archive_existing_versions, run):
+    try:
+       model =  client.create_registered_model(model_name)
+    except RestException as e:
+       model =  client.get_registered_model(model_name)
+    #model_artifact = "artifacts/sklearn-model"
+    model_artifact = "sklearn-model"
+    source = f"{run.info.artifact_uri}/{model_artifact}"
+    version = client.create_model_version(model_name, source, run.info.run_id)
+    if model_version_stage:
+        client.transition_model_version_stage(model_name, version.version, model_version_stage, archive_existing_versions)
+
+# COMMAND ----------
+
+with mlflow.start_run(run_name=f"sklearn_{mlflow.__version__}") as run:
     run_id = run.info.run_id
     print("MLflow:")
     print("  run_id:",run_id)
@@ -123,7 +144,10 @@ with mlflow.start_run(run_name="sklearn") as run:
     mlflow.log_param("max_depth", max_depth)
     mlflow.log_param("max_leaf_nodes", max_leaf_nodes)
         
-    mlflow.sklearn.log_model(model, "sklearn-model", signature=signature, registered_model_name=registered_model)
+    #mlflow.sklearn.log_model(model, "sklearn-model", signature=signature, model_name_name=model_name)
+    mlflow.sklearn.log_model(model, "sklearn-model", signature=signature)
+    if model_name:
+        _register_model(model_name, model_version_stage, archive_existing_versions, run)
         
     rmse = np.sqrt(mean_squared_error(test_y, predictions))
     r2 = r2_score(test_y, predictions)
@@ -141,11 +165,16 @@ display_run_uri(run.info.experiment_id, run_id)
 
 # COMMAND ----------
 
+if model_name:
+    display_registered_model_uri(model_name)
+
+# COMMAND ----------
+
 # MAGIC %md ### Predict
 
 # COMMAND ----------
 
-model_uri = f"runs:/{run_id}/{model_name}"
+model_uri = f"runs:/{run_id}/sklearn-model"
 model_uri
 
 # COMMAND ----------
