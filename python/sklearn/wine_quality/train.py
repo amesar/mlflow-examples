d38984e536ac5102ccd495fdafe487a418378009
amesar <amesar@users.noreply.github.co>
2022-12-03 14:31:11 -0500

    sklearn: added set_tags to experiment

--- python/sklearn/wine_quality/train.py
+++ python/sklearn/wine_quality/train.py
@@ -7,19 +7,23 @@ Modeling wine preferences by data mining from physicochemical properties. In Dec
 """
 
 import platform
+import time
 import pandas as pd
 import numpy as np
 import click
 import shortuuid
+
 import sklearn
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
+
 import mlflow
 import mlflow.sklearn
 from mlflow.models.signature import infer_signature
 from mlflow.exceptions import RestException
 from wine_quality import plot_utils
+
 
 print("Versions:")
 print("  MLflow Version:", mlflow.__version__)
@@ -33,6 +37,8 @@ client = mlflow.client.MlflowClient()
 
 col_label = "quality"
 
+ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+
 class Trainer():
     def __init__(self, experiment_name, data_path, log_as_onnx, save_signature, run_origin=None):
         self.experiment_name = experiment_name
@@ -44,6 +50,9 @@ class Trainer():
 
         if self.experiment_name:
             mlflow.set_experiment(experiment_name)
+            exp = client.get_experiment_by_name(experiment_name)
+            client.set_experiment_tag(exp.experiment_id, "version_mlflow", mlflow.__version__)
+            client.set_experiment_tag(exp.experiment_id, "experiment_created", ts)
 
 
     def _build_data(self, data_path):
@@ -61,7 +70,9 @@ class Trainer():
 
     def _register_model(self, mlflow_model_name, registered_model_name, registered_model_version_stage, archive_existing_versions, run):
         try:
-            client.create_registered_model(registered_model_name)
+            desc = "Skearn Wine Quality model"
+            tags = { "info": desc}
+            client.create_registered_model(registered_model_name, tags, desc)
         except RestException:
             pass
         source = f"{run.info.artifact_uri}/{mlflow_model_name}"
@@ -69,26 +80,26 @@ class Trainer():
         version = client.create_model_version(registered_model_name, source, run.info.run_id)
         if registered_model_version_stage:
             client.transition_model_version_stage(registered_model_name, version.version, registered_model_version_stage, archive_existing_versions)
+        desc = f"v{version.version} {registered_model_version_stage}"
+        client.update_model_version(registered_model_name, version.version, desc)
+        client.set_model_version_tag(registered_model_name, version.version, "info", desc)
 
 
     def train(self, registered_model_name, registered_model_version_stage="None", archive_existing_versions=True, output_path=None, max_depth=None, max_leaf_nodes=32):
-        import time
-        dt = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
-        run_name = f"{self.run_origin} {mlflow.__version__} {dt}" if self.run_origin else None
+        run_name = f"{self.run_origin} {mlflow.__version__} {ts}" if self.run_origin else None
         with mlflow.start_run(run_name=run_name) as run: # NOTE: when running with `mlflow run`, mlflow --run-name option takes precedence!
             run_id = run.info.run_id
             experiment_id = run.info.experiment_id
             print("MLflow:")
             print("  run_id:", run_id)
             print("  experiment_id:", experiment_id)
-            client.set_experiment_tag(experiment_id,"version_mlflow",mlflow.__version__)
             print("  experiment_name:", client.get_experiment(experiment_id).name)
 
             # MLflow tags
-            mlflow.set_tag("save_signature",self.save_signature)
+            mlflow.set_tag("save_signature", self.save_signature)
             mlflow.set_tag("data_path", self.data_path)
-            mlflow.set_tag("registered_model_name",registered_model_name)
-            mlflow.set_tag("registered_model_version_stage",registered_model_version_stage)
+            mlflow.set_tag("registered_model_name", registered_model_name)
+            mlflow.set_tag("registered_model_version_stage", registered_model_version_stage)
             mlflow.set_tag("uuid",shortuuid.uuid())
             mlflow.set_tag("run_origin", self.run_origin)
             mlflow.set_tag("version.mlflow", mlflow.__version__)
