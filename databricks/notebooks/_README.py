bf043be3e282a7a8332fc112bca874d080a905af
amesar <amesar>
2023-04-11 15:42:10 +0000

    Added databricks/notebooks/README.py

---
+++ databricks/notebooks/_README.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md ## README - Basic MLflow experiment notebooks
+# MAGIC 
+# MAGIC * [basic]($basic/_README)
+# MAGIC * [Mini_MLOps_Pipeline]($Mini_MLOps_Pipeline/_README)
+# MAGIC 
+# MAGIC Last updated: 2023-04-11
