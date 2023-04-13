4124138e68f762999658b428bd3cc4f95a4d1836
amesar <amesar>
2023-04-13 18:09:25 +0000

    Issue #10: enhanced basic/Sklearn_CustomModel notebook

--- databricks/notebooks/basic/_README.py
+++ databricks/notebooks/basic/_README.py
@@ -2,10 +2,12 @@
 # MAGIC %md ## Basic MLflow training notebooks
 # MAGIC 
 # MAGIC **ML training notebooks**
-# MAGIC * [02_Sklearn_Wine]($02_Sklearn_Wine)
+# MAGIC * [02_Sklearn_Wine]($02_Sklearn_Wine) - canonical Sklearn wine quality model showing most MLflow functionality
+# MAGIC * [Sklearn_CustomModel]($Sklearn_CustomModel) - examples of MLflow [Python custom models](https://mlflow.org/docs/latest/models.html#custom-python-models).
+# MAGIC 
 # MAGIC 
 # MAGIC **Other notebooks**
 # MAGIC * [Common]($Common)
 # MAGIC * [Versions]($Versions)
 # MAGIC 
-# MAGIC Last updated: 2023-03-27
+# MAGIC Last updated: 2023-04-13
