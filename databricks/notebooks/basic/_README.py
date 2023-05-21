00297b764fd3c9f3d02a3423252b188b9308f25f
amesar <amesar>
2023-05-21 16:33:44 +0000

    Notebook basic/_README update

--- databricks/notebooks/basic/_README.py
+++ databricks/notebooks/basic/_README.py
@@ -1,17 +1,22 @@
 # Databricks notebook source
 # MAGIC %md ## Basic MLflow training notebooks
-# MAGIC 
-# MAGIC **MLflow training and prediction notebooks**
+# MAGIC
+# MAGIC **Wine Quality model notebooks**
 # MAGIC * [Sklearn_Wine]($Sklearn_Wine) - canonical Sklearn wine quality model showing most MLflow functionality
 # MAGIC   * [Sklearn_Wine_ONNX]($Sklearn_Wine_ONNX) - ONNX version
-# MAGIC * [XGBoost_Wine]($XGBoost_Wine) - XGBoost wine quality model
+# MAGIC * [XGBoost_Wine]($XGBoost_Wine)
+# MAGIC * [AutoML_Wine]($AutoML_Wine)
 # MAGIC * [SparkML_Wine]($SparkML_Wine) - SparkML wine quality model
-# MAGIC * [Keras_Mnist]($Keras_Mnist) - TensorFlow Keras MNIST
+# MAGIC
+# MAGIC **Other Sklearn model notebooks**
 # MAGIC * [Sklearn_CustomModel]($Sklearn_CustomModel) - examples of three MLflow [Python custom models](https://mlflow.org/docs/latest/models.html#custom-python-models)
 # MAGIC * [Sklearn_Iris]($Sklearn_Iris) 
-# MAGIC 
-# MAGIC **Other notebooks**
-# MAGIC * [Common]($Common)
-# MAGIC * [Versions]($Versions)
-# MAGIC 
-# MAGIC Last updated: 2023-04-17
+# MAGIC
+# MAGIC **Other model notebooks**
+# MAGIC * [Keras_Mnist]($Keras_Mnist) - TensorFlow Keras MNIST
+# MAGIC
+# MAGIC **Helper notebooks**
+# MAGIC * [Common]($Common) - shared logic
+# MAGIC * [Versions]($Versions) - show version information
+# MAGIC
+# MAGIC Last updated: 2023-05-21
