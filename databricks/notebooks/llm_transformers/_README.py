91ad7a0b4a788ead4d2590d05cf1cb0c23ec7656
amesar <amesar>
2023-07-30 13:39:42 +0000

    Issue #16: Renamed notebook Text_to_Text_Transformer_Simple to Text_to_Text_Generation_Task

--- databricks/notebooks/llm_transformers/_README.py
+++ databricks/notebooks/llm_transformers/_README.py
@@ -5,11 +5,11 @@
 # MAGIC
 # MAGIC |Task |  Hugging Face Model | Notebook |
 # MAGIC |-----|-----|-------|
-# MAGIC | text2text-generation | [declare-lab/flan-alpaca-base](https://huggingface.co/declare-lab/flan-alpaca-base) | [Text_to_Text_Transformer_Simple]($Text_to_Text_Transformer_Simple)  |
-# MAGIC | conversational | [microsoft/DialoGPT-medium](https://huggingface.co/microsoft/DialoGPT-medium) | [Conversational_Simple]($Conversational_Simple)  |
-# MAGIC | feature-extraction | [sentence-transformers/all-MiniLM-L12-v2](https://huggingface.co/sentence-transformers/all-MiniLM-L12-v2) | [Sentence_Transformer_Simple]($Sentence_Transformer_Simple)  |
-# MAGIC | translation_en_to_fr | [t5-small](https://huggingface.co/t5-small) | [Translation_Simple]($Translation_Simple)  |
-# MAGIC | automatic-speech-recognition | [openai/whisper-tiny](https://huggingface.co/openai/whisper-tiny) | [Whisper_Simple]($Whisper_Simple)  |
+# MAGIC | text2text-generation | [declare-lab/flan-alpaca-base](https://huggingface.co/declare-lab/flan-alpaca-base) | [Text_to_Text_Generation_Task]($Text_to_Text_Generation_Task)  |
+# MAGIC | conversational | [microsoft/DialoGPT-medium](https://huggingface.co/microsoft/DialoGPT-medium) | [Conversational_Task]($Conversational_Task)  |
+# MAGIC | feature-extraction | [sentence-transformers/all-MiniLM-L12-v2](https://huggingface.co/sentence-transformers/all-MiniLM-L12-v2) | [Sentence_Transformer_Task]($Sentence_Transformer_Task)  |
+# MAGIC | translation_en_to_fr | [t5-small](https://huggingface.co/t5-small) | [Translation_Task]($Translation_Task)  |
+# MAGIC | automatic-speech-recognition | [openai/whisper-tiny](https://huggingface.co/openai/whisper-tiny) | [Whisper]($Whisper)  |
 # MAGIC
 # MAGIC ##### Other notebooks
 # MAGIC * [Common]($Common)
