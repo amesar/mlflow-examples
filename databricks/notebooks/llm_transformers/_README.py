69a4c31e46d97ea58def8cce9af37e6d73615c57
amesar <amesar>
2023-08-19 03:55:30 +0000

    llm_transformers/README: added some model sizes

--- databricks/notebooks/llm_transformers/_README.py
+++ databricks/notebooks/llm_transformers/_README.py
@@ -8,13 +8,13 @@
 # MAGIC
 # MAGIC ##### Transformer model notebooks
 # MAGIC
-# MAGIC | Notebook | Hugging Face Task | Hugging Face Model |
-# MAGIC |-----|-----|-------|
-# MAGIC | [Text_to_Text_Generation_Task]($Text_to_Text_Generation_Task) | [text2text-generation](https://huggingface.co/tasks/text-generation) | [declare-lab/flan-alpaca-base](https://huggingface.co/declare-lab/flan-alpaca-base) | 
-# MAGIC | [Translation_Task]($Translation_Task) | [translation_en_to_fr](https://huggingface.co/tasks/translation) | [t5-small](https://huggingface.co/t5-small) |  
-# MAGIC | [Conversational_Task]($Conversational_Task) | [conversational](https://huggingface.co/tasks/conversational) | [microsoft/DialoGPT-medium](https://huggingface.co/microsoft/DialoGPT-medium) |
-# MAGIC | [Feature_Extraction_Task]($Feature_Extraction_Task) | [feature-extraction](https://huggingface.co/tasks/feature-extraction) | [sentence-transformers/all-MiniLM-L12-v2](https://huggingface.co/sentence-transformers/all-MiniLM-L12-v2) |
-# MAGIC | [Speech_Recognition_Task]($Speech_Recognition_Task) | [automatic-speech-recognition](https://huggingface.co/tasks/automatic-speech-recognition) | [openai/whisper-tiny](https://huggingface.co/openai/whisper-tiny) | 
+# MAGIC | Notebook | Hugging Face Task | Hugging Face Model | Model GB|
+# MAGIC |-----|-----|-------|---|
+# MAGIC | [Text_to_Text_Generation_Task]($Text_to_Text_Generation_Task) | [text2text-generation](https://huggingface.co/tasks/text-generation) | [declare-lab/flan-alpaca-base](https://huggingface.co/declare-lab/flan-alpaca-base) | 1.65 |
+# MAGIC | [Translation_Task]($Translation_Task) | [translation_en_to_fr](https://huggingface.co/tasks/translation) | [t5-small](https://huggingface.co/t5-small) |  1.10 |
+# MAGIC | [Conversational_Task]($Conversational_Task) | [conversational](https://huggingface.co/tasks/conversational) | [microsoft/DialoGPT-medium](https://huggingface.co/microsoft/DialoGPT-medium) | xx |
+# MAGIC | [Feature_Extraction_Task]($Feature_Extraction_Task) | [feature-extraction](https://huggingface.co/tasks/feature-extraction) | [sentence-transformers/all-MiniLM-L12-v2](https://huggingface.co/sentence-transformers/all-MiniLM-L12-v2) | xx |
+# MAGIC | [Speech_Recognition_Task]($Speech_Recognition_Task) | [automatic-speech-recognition](https://huggingface.co/tasks/automatic-speech-recognition) | [openai/whisper-tiny](https://huggingface.co/openai/whisper-tiny) |  xx |
 # MAGIC
 # MAGIC ##### Other notebooks
 # MAGIC * [Run_Task_Notebooks]($Run_Task_Notebooks) - Run the task notebook suite.
@@ -24,4 +24,4 @@
 # MAGIC ##### Source origin
 # MAGIC * https://github.com/mlflow/mlflow/blob/master/examples/transformers
 # MAGIC
-# MAGIC Last updated: 2023-08-01
+# MAGIC Last updated: 2023-08-18
