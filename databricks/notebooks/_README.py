643c0a7411759e3b6bfbfaaae74febf761aa5824
amesar <amesar>
2023-08-19 03:58:42 +0000

    Rename notebook folder from ll_transformers transformers

--- databricks/notebooks/_README.py
+++ databricks/notebooks/_README.py
@@ -2,7 +2,7 @@
 # MAGIC %md ## README - MLflow Model Notebooks
 # MAGIC
 # MAGIC * [basic]($basic/_README) - Basic MLflow model notebooks
-# MAGIC * [llm_transformers]($llm_transformers/_README) - Basic transformer examples
+# MAGIC * [transformers]($transformers/_README) - Basic transformer examples
 # MAGIC * [Mini_MLOps_Pipeline]($Mini_MLOps_Pipeline/_README)
 # MAGIC
-# MAGIC Last updated: 2023-07-30
+# MAGIC Last updated: 2023-08-18
