20f5ad75ad4e3f552a78545d35be4012d85fd6d7
amesar <amesar>
2023-11-16 03:38:57 +0000

    Sklearn Wine Feature Store notebook renames with leading integer

--- databricks/notebooks/basic/feature_store/_README.py
+++ databricks/notebooks/basic/feature_store/_README.py
@@ -2,9 +2,9 @@
 # MAGIC %md ### README - Sklearn Wine Feature Store 
 # MAGIC
 # MAGIC **Notebooks**
-# MAGIC * [Sklearn_Wine_FS]($Sklearn_Wine_FS) - Standard Sklearn train and rpedict using feature store table.
-# MAGIC * [Sklearn_Wine_FS_Prep]($Sklearn_Wine_FS_Prep) - Run this notebook first once to create feature table.
-# MAGIC * [Sklearn_Wine_FS_Details]($Sklearn_Wine_FS_Details) - Get details of feature table.
+# MAGIC * [01_Sklearn_Wine_FS_Prep]($01_Sklearn_Wine_FS_Prep) - Run this notebook first once to create feature table.
+# MAGIC * [02_Sklearn_Wine_FS_Details]($02_Sklearn_Wine_FS_Details) - Get details of feature table.
+# MAGIC * [03_Sklearn_Wine_FS]($03_Sklearn_Wine_FS) - Standard Sklearn train and predict using feature store table.
 # MAGIC * [Common]($Common) - Common for feature store
 # MAGIC
-# MAGIC Last updated: 2023-08-16
+# MAGIC Last updated: 2023-11-15
