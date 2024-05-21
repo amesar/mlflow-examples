3f8ba7fcd7b718430cce2d6e808a8b9852715e45
amesar <amesar@users.noreply.github.co>
2024-05-20 22:34:35 -0400

    sklearn: updated setup.py and conda.yaml

--- python/sklearn/setup.py
+++ python/sklearn/setup.py
@@ -8,7 +8,7 @@ setup(name='mlflow-sklearn-wine',
       zip_safe=False,
       python_requires=">=3.8",
       install_requires=[
-          "mlflow>=2.3.0",
+          "mlflow>=2.13.2
           "scikit-learn",
           "matplotlib",
           "onnx==1.15.0",
